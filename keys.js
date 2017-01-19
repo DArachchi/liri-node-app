ea0c3b6a7de4e1e4f7ca88f89b78e8e852fbd0bf
DArachchi <DArachchi@gmail.com>
2017-01-18 18:22:21 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: 'xM9UIMxsTlUf73bu4m5xt1Sej',
-  consumer_secret: 'b0A8wtzv7l615kF5uMXdMLHlCzum4EO0n9aWQbPeFYSiPZsEli',
-  access_token_key: '950421049-qE9ecpThbn8KCiTLu8seh1VuGxPrDmzZpaSi5TxG',
-  access_token_secret: 'jCNn33PBEWcImeYZ3QfB2p137o0avuLLXZTXY3JSJj1nx',
-}
