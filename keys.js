fad5b000d44c1f3bee0e6d4ffdda847beff6fe94
Dino Arachchi <Dino Arachchi>
2017-01-18 18:21:28 -0600

    Corrected twitter keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
-  consumer_key: 'dls9X5pyXchCg0RxBQvQdvx7J',
-  consumer_secret: '14lSM1vwA9kSfmVl7TJzqQUk6hs7rGySFYxKGzxVeb77lEFnbr',
+  consumer_key: 'xM9UIMxsTlUf73bu4m5xt1Sej',
+  consumer_secret: 'b0A8wtzv7l615kF5uMXdMLHlCzum4EO0n9aWQbPeFYSiPZsEli',
   access_token_key: '950421049-qE9ecpThbn8KCiTLu8seh1VuGxPrDmzZpaSi5TxG',
   access_token_secret: 'jCNn33PBEWcImeYZ3QfB2p137o0avuLLXZTXY3JSJj1nx',
 }
